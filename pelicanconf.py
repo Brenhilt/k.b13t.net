3749f8a349fce7955c93eab606adcbe36b8ab7d3
Brenhilt Schildt <register+git@brenhilt.com>
2019-11-08 11:26:02 +0900

    initial commit

---
+++ pelicanconf.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = '__Bren'
+SITENAME = 'かんこれメモ'
+SITEURL = ''
+THEME = "theme/blueidea"
+PATH = 'content'
+TIMEZONE = 'Asia/Tokyo'
+DEFAULT_LANG = 'ja'
+
+DATE_FORMATS = {
+    "ja": "%Y/%m/%d(%a)",
+}
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ('艦これ', 'http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/'),
+)
+
+# Social widget
+SOCIAL = (
+)
+
+DEFAULT_PAGINATION = False
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
